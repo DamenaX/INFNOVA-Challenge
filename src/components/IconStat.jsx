de408c6799be8052c1f5eb3689f2c61162e86991
DamenaX <danielalemu011@gmail.com>
2026-02-20 15:36:52 +0300

    Add reusable Icon stat component

---
+++ src/components/IconStat.jsx
@@ -0,0 +1,23 @@
+/* Note on Usage: In the calling (rendering) file
+        1. Import the iconlibrary (import icons from '...iconLibrary.jsx) 
+        2. then use icon dot syntax to see the list of all icons (eg: icons.clock)
+    To add icons to the icon library, simply import their svg files in the IconLibrary.jsx file
+*/
+
+function IconStat({ icon, field, value, variant = "default-hero" }) {
+    const variants = {
+        "default-hero": "text-text-on-hero",
+        "course-info": "text-text-muted"
+    }
+    const Icon = icon;
+    return (
+        <div className={`flex space-x-2 items-center ${variants[variant]}`}>
+            <Icon className="h-5 aspect-square"/>
+            <p className={`text-[16px]  flex`}>
+                <span className="font-normal">{field}:</span>&nbsp;<span>{value}</span>
+            </p>
+        </div>
+    )
+}
+
+export default IconStat
