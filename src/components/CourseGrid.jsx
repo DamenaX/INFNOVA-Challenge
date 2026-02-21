3941808f8cb86e8e42ed5a5e8c22c490a402aca5
DamenaX <danielalemu011@gmail.com>
2026-02-21 22:24:44 +0300

    Improve spacing on the search bar

--- src/components/CourseGrid.jsx
+++ src/components/CourseGrid.jsx
@@ -1,6 +1,6 @@
 function CourseGrid({children}) {
     return (
-        <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full px-6">
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(280px,1fr))] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
             {children}
         </div>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,15 +8,15 @@ function SearchBar() {
             <div className="flex flex-wrap space-x-2 space-y-2 p-6 w-full bg-surface-primary shadow-nova-sm rounded-[10px] h-full">
 
                 {/* Search field */}
-                <div className="flex items-center space-x-2 flex-1 basis-[70%] px-5 py-2 border border-border-default rounded-xl font-normal text-text-muted text-[16px] focus-within:border-text-accent focus-within:text-text-secondary min-w-70 h-10.5">
+                <div className="flex items-center space-x-2 flex-70 px-5 py-2 border border-border-default rounded-xl font-normal text-text-muted text-[16px] focus-within:border-text-accent focus-within:text-text-secondary min-w-70 h-10.5">
                     <SearchIcon className="" />
                     <input type="text" name="query" placeholder="Search courses, instructors..." className="placeholder:text-text-tertiary/80 w-full outline-none" />
                 </div>
 
                 {/* Fields that were not clarified in the figma design? */}
-                <div className="flex flex-1 space-x-2 h-10.5">
-                    <div className="basis-[10%] px-10 py-2 border border-border-default rounded-[10px] h-full"></div>
-                    <div className="basis-[20%] px-10 py-2 border border-border-default rounded-[10px] h-full"></div>
+                <div className="flex flex-30 space-x-2 h-10.5">
+                    <div className="basis-[10%] px-10 py-2 border border-border-default rounded-[10px] h-full flex-30 max-w-32.5"></div>
+                    <div className="basis-[20%] px-10 py-2 border border-border-default rounded-[10px] h-full flex-70 max-w-50"></div>
                 </div>
 
             </div>
