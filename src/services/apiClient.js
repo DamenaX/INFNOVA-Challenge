84ff1b2eeb6aae1c2265926f832e91137b6e928b
DamenaX <danielalemu011@gmail.com>
2026-02-22 23:54:23 +0300

    Add API client

---
+++ src/services/apiClient.js
@@ -0,0 +1,16 @@
+// Error handling and loading logic in useFetch.js
+
+const BASE_URL = 'https://infnova-course-api.vercel.app/api'
+
+export async function getCourses() {
+    const res = await fetch(`${BASE_URL}/courses`)
+    if (!res.ok) throw new Error('Failed to fetch courses')
+    return res.json()
+}
+
+export async function getCourseDetails(id) {
+    const res = await fetch(`${BASE_URL}/courses/${id}`)
+    if (!res.ok) throw new Error('Failed to fetch course')
+    return res.json()
+}
+
