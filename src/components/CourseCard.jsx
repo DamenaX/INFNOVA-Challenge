9312c5a5312a72ca2a0b420846315340e01015a4
DamenaX <danielalemu011@gmail.com>
2026-02-23 07:18:03 +0300

    Fix images not rendering due to tailwindCSS compilers inability to use dynamic background images

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -8,16 +8,16 @@ function CourseCard({ course }) {
     return (
         <div className="flex flex-col rounded-xl overflow-hidden shadow-nova-md flex-none min-w-[280px] w-full h-full">
             {/* course image contianer */}
-            <div className={`aspect-330/192 flex flex-1/2 justify-end p-2 bg-${course.thumbnail} bg-cover bg-center bg-no-repeat`}>
-                <Badge level='beginner' />
+            <div className="h-48 flex-shrink-0 flex justify-end p-2 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${course.thumbnail})` }}>
+                <Badge level={course.level} />
             </div>
 
             {/* course details container */}
-            <div className="flex flex-1/2 flex-col p-5 space-y-4">
+            <div className="flex basis-1/2 flex-col p-5 space-y-4">
                 {/* text information */}
                 <div className="flex flex-col space-y-4">
                     <div className="flex flex-col space-y-3">
-                        <p className="text-text-accent font-normal text-[12px]">{course.category}</p>
+                        <p className="text-text-accent font-normal text-[12px] uppercase">{course.category}</p>
                         <h3 className="text-[18px]">{course.title}</h3>
                     </div>
                     <p className="text-text-secondary text-[14px]">Instructor: <span className="font-normal">{course.instructor}</span></p>
