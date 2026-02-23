b2865b7dc70318c9a08621816f83ff323dc484c2
DamenaX <danielalemu011@gmail.com>
2026-02-23 07:19:00 +0300

    Adjust the badge to the API inputs

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,29 +1,17 @@
-//to change variant level option (beginner) to UI level (Beginner)
-function capitalize(str) {
-    if (!str) return "";
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-function Badge({level = "default"}) {
+function Badge({ level = "Default" }, onHero = false) {
     const base = "flex items-center justify-center font-normal text-[12px] rounded-full px-3 py-1 w-fit h-fit";
-
-    /* The funciton will be able to take strings like "intermediate level xyz" */
-    let variant
-    if (level.includes("beginner")) variant = "beginner";
-    if (level.includes("intermediate")) variant = "intermediate";
-    if (level.includes("advanced")) variant = "advanced";
 
 
     const variants = {
-        "default": "bg-gray-100 text-gray-800",
-        "beginner": "bg-badge-beginner-bg text-badge-beginner-text",
-        "intermediate": "bg-badge-intermediate-bg text-badge-intermediate-text",
-        "advanced": "bg-badge-advanced-bg text-badge-advanced-text"
+        "Default": "bg-gray-100 text-gray-800",
+        "Beginner": "bg-badge-beginner-bg text-badge-beginner-text",
+        "Intermediate": "bg-badge-intermediate-bg text-badge-intermediate-text",
+        "Advanced": "bg-badge-advanced-bg text-badge-advanced-text"
     }
 
     return (
-        <div className={`${base} ${variants[variant]}`}>
-            <span className="w-fit h-fit">{capitalize(level)}</span>
+        <div className={`${base} ${variants[level]}`}>
+            <span className="w-fit h-fit ">{level} {onHero ? "Level" : ""}</span>
         </div>
     )
 }
