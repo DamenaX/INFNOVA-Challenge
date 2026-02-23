718e57511bf4400cde93d0d6ebbdde6f5d660fdc
DamenaX <danielalemu011@gmail.com>
2026-02-23 07:17:20 +0300

    Add API integration to Course List page

--- src/pages/CourseListPage.jsx
+++ src/pages/CourseListPage.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import useFetch from '../services/useFetch'
+import { getCourses } from '../services/apiClient'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import CourseGrid from '../components/CourseGrid'
@@ -7,28 +9,30 @@ import SearchBar from '../components/SearchBar'
 import CoursesContainer from '../components/CoursesContainer'
 import HeroSection from '../components/HeroSection'
 import MainContainer from '../components/MainContainer'
+import CourseCardSkeleton from '../components/skeletons/CourseCardSkeleton'
 
 
 
 
 
 function CourseListPage() {
-  const [count, setCount] = useState(0)
+  const { data: courses, loading, error } = useFetch(getCourses)
+
 
   return (
     <>
       <HeroSection variant="main"
-        title="Explore Our Courses"
-        description="Master new skills with expert-led courses designed for the modern learner. Start your learning journey today with INFNOVA Academy."
+        heading="Explore Our Courses"
+        subheading="Master new skills with expert-led courses designed for the modern learner. Start your learning journey today with INFNOVA Academy."
       />
 
       <CoursesContainer>
         <SearchBar />
         <CourseGrid>
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
+          {loading
+            ? Array(6).fill(null).map((_, i) => <CourseCardSkeleton key={i} />)
+            : courses.map(course => <CourseCard key={course.id} course={course} />)
+          }
         </CourseGrid>
       </CoursesContainer>
     </>
